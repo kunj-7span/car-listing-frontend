cde8fa5fa6ac1f0e6b0150617bdfc683ad6b258c
kunj-7span <kunj@7span.com>
2026-06-12 02:06:43 +0530

    [fix]: layput for footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,9 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <TooltipProvider>
           <Header />
-          {children}
+          <main className="flex-1">
+            {children}
+          </main>
           <Footer />
         </TooltipProvider>
       </body>
