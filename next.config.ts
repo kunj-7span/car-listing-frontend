c9a0364d9d26bdfde520247c327192737ba7b012
kunj-7span <kunj@7span.com>
2026-06-14 13:18:43 +0530

    [feat]: update image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,15 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "marketplace-cdn.cars24.com",
+      },
+      {
+        protocol: "https",
         hostname: "media.cars24.com",
+      },
+      {
+        protocol: "https",
+        hostname: "c2c-marketplace.s3.ap-south-1.amazonaws.com",
       },
     ],
   },
